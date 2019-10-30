9db6109257eecf556230fddac178237f32880719
jefson19 <desenvolvimento2.techmobil@gmail.com>
2019-10-30 13:55:49 -0300

    first commit

---
+++ src/pages/User/index.js
@@ -0,0 +1,28 @@
+import React, {Component} from 'react';
+import {View} from 'react-native';
+import api from '../../services/api';
+
+// import { Container } from './styles';
+
+export default class User extends Component {
+  static navigationOptions = ({navigation}) => ({
+    title: navigation.getParam('user').name,
+  });
+
+  state = {
+    stars: [],
+  };
+
+  async componentDidMount() {
+    const {navigation} = this.props;
+    const user = navigation.getParam('user');
+
+    const response = await api.get(`/users/${user.login}/starred`);
+
+    this.setState({stars: response.data});
+  }
+
+  render() {
+    return <View />;
+  }
+}
