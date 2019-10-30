48c989eb3072e1e7cc4c032c10fcacf5bdfdd22e
Jefson19 <jefsonmatos.ti@gmail.com>
2019-10-30 15:57:50 -0300

    finalizando projeto

--- src/pages/User/index.js
+++ src/pages/User/index.js
@@ -1,13 +1,31 @@
 import React, {Component} from 'react';
-import {View} from 'react-native';
+import PropTypes from 'prop-types';
 import api from '../../services/api';
 
-// import { Container } from './styles';
+import {
+  Container,
+  Header,
+  Avatar,
+  Name,
+  Bio,
+  Stars,
+  Starred,
+  OwnerAvatar,
+  Info,
+  Title,
+  Author,
+} from './styles';
 
 export default class User extends Component {
   static navigationOptions = ({navigation}) => ({
     title: navigation.getParam('user').name,
   });
+
+  static propTypes = {
+    navigation: PropTypes.shape({
+      getParam: PropTypes.func,
+    }).isRequired,
+  };
 
   state = {
     stars: [],
@@ -23,6 +41,31 @@ export default class User extends Component {
   }
 
   render() {
-    return <View />;
+    const {navigation} = this.props;
+    const {stars} = this.state;
+    const user = navigation.getParam('user');
+    return (
+      <Container>
+        <Header>
+          <Avatar source={{uri: user.avatar}} />
+          <Name>{user.name}</Name>
+          <Bio>{user.bio}</Bio>
+        </Header>
+
+        <Stars
+          data={stars}
+          keyExtractor={star => String(star.id)}
+          renderItem={({item}) => (
+            <Starred>
+              <OwnerAvatar source={{uri: item.owner.avatar_url}} />
+              <Info>
+                <Title>{item.name}</Title>
+                <Author>{item.owner.login}</Author>
+              </Info>
+            </Starred>
+          )}
+        />
+      </Container>
+    );
   }
 }
